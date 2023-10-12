1751979df07cef5ef41f77921e76cf683e321728
9791619582 <144415978+9791619582@users.noreply.github.com>
2023-10-12 09:31:21 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+class BankAccount:
+    def _init_(self, account_number, account_holder_name, initial_balance):
+        self.__account_number = account_number
+        self.__account_holder_name = account_holder_name
+        self.__account_balance = initial_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+            print(f"Deposited ${amount}. New balance: ${self.__account_balance}")
+        else:
+            print("Invalid deposit amount. Please enter a positive value.")
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.__account_balance:
+            self.__account_balance -= amount
+            print(f"Withdrew ${amount}. New balance: ${self.__account_balance}")
+        else:
+            print("Insufficient funds or invalid withdrawal amount.")
+
+    def display_balance(self):
+        print(f"Account balance for {self._account_holder_name}: ${self._account_balance}")
+
+
+# Example usage:
+if _name_ == "_main_":
+    # Create an instance of BankAccount
+    my_account = BankAccount("123456789", "John Doe", 1000.0)
+
+    # Deposit and withdraw money
+    my_account.deposit(500)
+    my_account.withdraw(200)
+    my_account.display_balance()
